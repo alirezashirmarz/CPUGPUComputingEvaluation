449906fdaf444c167d63a9344b61e30025f3bfdc
Nahid <n.shahab85@gmail.com>
2023-07-07 00:36:54 +0330

    plot_average_from_json was created

--- PlotLineChart.py
+++ PlotLineChart.py
@@ -14,15 +14,17 @@ def plot_line_chart_from_json(json_filename):
     execution_times = []
 
     if json_filename=='averaged_execution_cpu_times.json':
-        lineChart_name = "lineChart_matrix_cpu.png"    
+        lineChart_name = "lineChart_matrix_cpu.png"   
+        lineColor = 'red'   
     else:
        lineChart_name = "lineChart_matrix_gpu.png"    
+       lineColor = 'blue'
 
     for size, result in data.items():
         sizes.append(int(result['size']))
         execution_times.append(float(result['average_execution_time']))
 
-    plt.plot(sizes, execution_times)
+    plt.plot(sizes, execution_times,color=lineColor)
     plt.xlabel('Size')
     plt.ylabel('Average Execution Time')
     plt.title('Averaged Execution Times - Line Plot')
